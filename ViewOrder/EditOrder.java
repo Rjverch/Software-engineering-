1237b45baf19823238c3ffaba3c5f770040da6b1
Oliver4096 <oliver.jiang@ttu.edu>
2020-02-13 18:31:22 -0600

    Update EditOrder.java

--- ViewOrder/EditOrder.java
+++ ViewOrder/EditOrder.java
@@ -1,20 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class EditOrder {
 	
-	public void Editorder(String path) {
+	public void Editorder(String path, int ordernum) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter new order details:");
-		String order = sc.nextLine();
+		
 		try {
+			BufferedReader readorder = new BufferedReader(new FileReader(path));
 			FileWriter orderwriter = new FileWriter(path);
-			orderwriter.write(order);
-			orderwriter.close();
-			System.out.println("order successfully edited");
-		}catch(Exception e) {
-			System.out.println("error in editing order");
+			String row;
+			while((row = readorder.readLine()) != null) {
+				String[] order = row.split(",");
+				if(Integer.parseInt(order[2]) == ordernum) {
+					System.out.println("Enter new order details:");
+					String neworder = sc.nextLine();
+					orderwriter.append(neworder);
+					sc.close();
+					readorder.close();
+					orderwriter.close();
+					return;
+				}
+			System.out.print("order not found");
+			return;
+			}
+		} catch (IOException e1) {
+			System.out.print("error")
 		}
-		sc.close();
+		
 	}
 }
